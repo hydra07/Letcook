7243866bf6f224679a6a0a294e3d6f33fb9d7eab
bluefishhh93 <xuanthanha03@gmail.com>
2023-10-20 02:18:26 +0700

    20/10thanh re-update recipe

--- Library/src/main/java/com/ecommerce/library/service/IngredientService.java
+++ Library/src/main/java/com/ecommerce/library/service/IngredientService.java
@@ -5,10 +5,13 @@ import com.ecommerce.library.model.Ingredient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public interface IngredientService {
     List<Ingredient> dtosToIngredients(List<IngredientDto> ingredientDto);
 
     List<IngredientDto> ingredientsToDtos(List<Ingredient> ingredient);
+
+    Map<String, Double> getNutrition(Ingredient ingredient);
 }
